620fc9fb201b35ca1a5ef0b0794e7578a6c2bc71
Prasenjit Das <132381731+prasenjitpriyan@users.noreply.github.com>
2023-11-20 22:28:15 +0530

    Update BookRoutes.js

--- routes/BookRoutes.js
+++ routes/BookRoutes.js
@@ -5,7 +5,7 @@ const BookController = require("../controllers/BookController");
 router.get("/", BookController.findAll);
 router.get("/:id", BookController.getBook);
 router.post("/", BookController.addBook);
-router.patch("/:id", BookController.updateBook)
+router.put("/:id", BookController.updateBook)
 router.delete("/:id", BookController.deleteBook)
 
 module.exports = router;
